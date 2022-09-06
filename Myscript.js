5eb9ed9e247019af5177cb2fbb219827c8509f3b
itiki89 <wieyo89@gmail.com>
2022-09-06 13:57:45 -0500

    Delete Myscript.js

--- Myscript.js
+++
@@ -1,5 +0,0 @@
-const weekday = ["Hoy es Domingo","Hoy es Lunes","Hoy es Martes","Hoy es Miercoles","Hoy es Jueves","Viernes","Hoy es Sabado"];
-
-const d = new Date();
-let day = weekday[d.getDay()];
-document.getElementById("diasemana").innerHTML = day;
